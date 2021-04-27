f47126139bec88d4de49899542ce060f3a1ae5c4
RajanKumar5 <rajan.kumar.rk.0502@gmail.com>
2021-04-27 11:33:09 +0530

    Updated

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    fun onDigit(view: View) {
+        binding.tvInput.append((view as Button).text)
+    }
+
+    fun onClear(view: View) {
+        binding.tvInput.text = ""
+    }
+
 }
